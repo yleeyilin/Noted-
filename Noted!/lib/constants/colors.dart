a1f71da6ddfcd6353ed215fb27c4d246fb4a2fbd
kayabuttertoastt <e0970063@u.nus.edu>
2023-05-15 16:10:48 +0800

    Modified Colors

--- Noted!/lib/constants/colors.dart
+++ Noted!/lib/constants/colors.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
 
+//Colors are labeled based on their usage
+
 var primary = const Color.fromARGB(255, 9, 9, 82);
 var inputbox = const Color.fromARGB(255, 174, 169, 169);
+var text = Colors.white;
