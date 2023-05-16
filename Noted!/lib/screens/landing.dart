4ba2611302bf3f73cbf44edf6366f8fdb66f46a1
kayabuttertoastt <e0970063@u.nus.edu>
2023-05-16 14:05:16 +0800

    Edited main.dart

---
+++ Noted!/lib/screens/landing.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Landing extends StatelessWidget {
+  const Landing({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    //temporary
+    return const Scaffold();
+  }
+}
