ab12c908b1a9cf0302925c0b7ba8d5232fbe3471
kayabuttertoastt <e0970063@u.nus.edu>
2023-05-18 13:00:22 +0800

    edited snackbar

--- noted/lib/authentication/showsnackbar.dart
+++ noted/lib/authentication/showsnackbar.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
 void showSnackBar(BuildContext context, String text) {
-  ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(text)));
+  ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      content: Text(text),
+    ),
+  );
 }
