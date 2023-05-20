dd0d70887a5b704ab2a4791e2bd1dfd79ec8ade9
Yi Lin <yiiiliiinn@gmail.com>
2023-05-20 21:38:14 +0800

    added neo4driver

--- noted/lib/view/login/signup.dart
+++ noted/lib/view/login/signup.dart
@@ -4,10 +4,11 @@ import 'package:noted/model/colors.dart';
 import 'package:noted/view/home.dart';
 import 'package:noted/view/login/login.dart';
 import 'package:noted/model/authentication/showsnackbar.dart';
+import 'package:neo4driver/neo4driver.dart';
 //import 'package:noted/view/verify.dart';
 
 class SignUp extends StatefulWidget {
-  const SignUp({super.key});
+  const SignUp({Key? key}) : super(key: key);
 
   @override
   State<SignUp> createState() {
@@ -16,174 +17,199 @@ class SignUp extends StatefulWidget {
 }
 
 class _SignUpState extends State<SignUp> {
+  
+  @override
+  void initState(){
+    NeoClient.withAuthorization( 
+      username: 'neo4j',  
+      password: 'CCE-9y4M1VWFvtaOIuli84-LhP6vMbniNQze5WrX7WE', 
+      databaseAddress: 'neo4j+s://f68363e2.databases.neo4j.io',
+      databaseName: 'Instance01',   
+    );
+
+    super.initState();
+  }
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
+  TextEditingController nameController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: Container(
-        decoration: BoxDecoration(
-          color: primary,
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            const SizedBox(height: 45),
-            Image.asset(
-              'assets/images/logo-darkblue.png',
-              height: 100,
-            ),
-            const SizedBox(height: 155),
-            const Center(
-              child: Text(
-                'Name',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
-                ),
+    return Scaffold(
+      body: Material(
+        child: Container(
+          decoration: BoxDecoration(
+            color: primary,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              const SizedBox(height: 45),
+              Image.asset(
+                'assets/images/logo-darkblue.png',
+                height: 100,
               ),
-            ),
-            const SizedBox(height: 5),
-            SizedBox(
-              width: 300,
-              child: TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(9),
+              const SizedBox(height: 155),
+              const Center(
+                child: Text(
+                  'Name',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
                   ),
-                  contentPadding:
-                      const EdgeInsets.symmetric(vertical: 10, horizontal: 30),
-                  filled: true,
-                  fillColor: inputbox,
                 ),
               ),
-            ),
-            const SizedBox(height: 20),
-            const Center(
-              child: Text(
-                'NUS Email',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
-                ),
-              ),
-            ),
-            const SizedBox(height: 5),
-            SizedBox(
-              width: 300,
-              child: TextField(
-                controller: emailController,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(9),
+              const SizedBox(height: 5),
+              SizedBox(
+                width: 300,
+                child: TextField(
+                  controller: nameController,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(9),
+                    ),
+                    contentPadding: const EdgeInsets.symmetric(
+                        vertical: 10, horizontal: 30),
+                    filled: true,
+                    fillColor: inputbox,
                   ),
-                  contentPadding:
-                      const EdgeInsets.symmetric(vertical: 10, horizontal: 30),
-                  filled: true,
-                  fillColor: inputbox,
                 ),
               ),
-            ),
-            const SizedBox(height: 20),
-            const Center(
-              child: Text(
-                'Password',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
-                ),
-              ),
-            ),
-            const SizedBox(height: 5),
-            SizedBox(
-              width: 300,
-              child: TextField(
-                controller: passwordController,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(9),
+              const SizedBox(height: 20),
+              const Center(
+                child: Text(
+                  'NUS Email',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
                   ),
-                  contentPadding:
-                      const EdgeInsets.symmetric(vertical: 10, horizontal: 50),
-                  filled: true,
-                  fillColor: inputbox,
                 ),
               ),
-            ),
-            const SizedBox(height: 20),
-            const Center(
-              child: Text(
-                'Confirm Password',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
-                ),
-              ),
-            ),
-            const SizedBox(height: 5),
-            SizedBox(
-              width: 300,
-              child: TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(9),
+              const SizedBox(height: 5),
+              SizedBox(
+                width: 300,
+                child: TextField(
+                  controller: emailController,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(9),
+                    ),
+                    contentPadding: const EdgeInsets.symmetric(
+                        vertical: 10, horizontal: 30),
+                    filled: true,
+                    fillColor: inputbox,
                   ),
-                  contentPadding:
-                      const EdgeInsets.symmetric(vertical: 10, horizontal: 50),
-                  filled: true,
-                  fillColor: inputbox,
                 ),
               ),
-            ),
-            const SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                OutlinedButton.icon(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => const Login()),
-                    );
-                  },
-                  style:
-                      OutlinedButton.styleFrom(foregroundColor: Colors.white),
-                  icon: const Icon(Icons.arrow_back),
-                  label: const Text('Back'),
-                ),
-                OutlinedButton.icon(
-                  onPressed: () {
-                    if (emailController.text.endsWith("@u.nus.edu")) {
-                      FirebaseAuth.instance
-                          .createUserWithEmailAndPassword(
-                              email: emailController.text,
-                              password: passwordController.text)
-                          .then((value) {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => const Home(),
-                          ),
-                        );
-                      }).catchError((error) {
-                        // Failed to create user
-                        showErrorSnackbar(context, error);
-                      });
-                    } else {
-                      // Invalid email domain
-                      showErrorSnackbar(context, "Invalid email domain");
-                    }
-                  },
-                  style: OutlinedButton.styleFrom(
-                    foregroundColor: Colors.white,
+              const SizedBox(height: 20),
+              const Center(
+                child: Text(
+                  'Password',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
                   ),
-                  icon: const Icon(Icons.arrow_right_outlined),
-                  label: const Text('Create'),
-                )
-              ],
-            ),
-          ],
+                ),
+              ),
+              const SizedBox(height: 5),
+              SizedBox(
+                width: 300,
+                child: TextField(
+                  controller: passwordController,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(9),
+                    ),
+                    contentPadding: const EdgeInsets.symmetric(
+                        vertical: 10, horizontal: 50),
+                    filled: true,
+                    fillColor: inputbox,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              const Center(
+                child: Text(
+                  'Confirm Password',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 5),
+              SizedBox(
+                width: 300,
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(9),
+                    ),
+                    contentPadding: const EdgeInsets.symmetric(
+                        vertical: 10, horizontal: 50),
+                    filled: true,
+                    fillColor: inputbox,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  OutlinedButton.icon(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => const Login()),
+                      );
+                    },
+                    style:
+                        OutlinedButton.styleFrom(foregroundColor: Colors.white),
+                    icon: const Icon(Icons.arrow_back),
+                    label: const Text('Back'),
+                  ),
+                  OutlinedButton.icon(
+                    onPressed: () {
+                      if (emailController.text.endsWith("@u.nus.edu")) {
+                        FirebaseAuth.instance
+                            .createUserWithEmailAndPassword(
+                                email: emailController.text,
+                                password: passwordController.text)
+                            .then((value) async {
+                          NeoClient().createNode(
+                            labels: ['User'],
+                            properties: {
+                              "name": nameController.text,
+                              "email": emailController.text,
+                            },
+                          );
+                          // ignore: use_build_context_synchronously
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const Home(),
+                            ),
+                          );
+                        }).catchError((error) {
+                          // Failed to create user
+                          print(error.toString());
+                          showErrorSnackbar(context, error);
+                        });
+                      } else {
+                        // Invalid email domain
+                        showErrorSnackbar(context, "Invalid email domain");
+                      }
+                    },
+                    style: OutlinedButton.styleFrom(
+                      foregroundColor: Colors.white,
+                    ),
+                    icon: const Icon(Icons.arrow_right_outlined),
+                    label: const Text('Create'),
+                  )
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
