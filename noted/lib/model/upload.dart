91a1d3ce40b5aec46e11992277d71e0b12da2abc
Yi Lin <yiiiliiinn@gmail.com>
2023-05-25 03:02:41 +0800

    added mutation funtions

--- noted/lib/model/upload.dart
+++ noted/lib/model/upload.dart
@@ -2,6 +2,42 @@ import 'dart:io';
 import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:noted/main.dart';
 
+
+Future<void> createNotesNode(String name, File pdfFile) async {
+  final rawContent = await pdfFile.readAsBytes();
+  final pdfContent = String.fromCharCodes(rawContent);
+
+  final MutationOptions options = MutationOptions(
+    document: gql('''
+      mutation CreateNotes(\$name: String!, \$content: String!) {
+        createNotes(input: { name: \$name, content: \$content }) {
+          success
+          message
+        }
+      }
+    '''),
+    variables: <String, dynamic>{
+      'name': name,
+      'content': pdfContent,
+    },
+  );
+
+  final QueryResult result = await client.value.mutate(options);
+
+  if (result.hasException) {
+    print('GraphQL Error: ${result.exception.toString()}');
+  } else {
+    final Map<String, dynamic>? data = result.data?['createNotes'];
+    if (data != null) {
+      final bool success = data['success'] as bool;
+      final String message = data['message'] as String;
+      print('Create Notes Success: $success');
+      print('Message: $message');
+    }
+  }
+}
+
+
 Future<void> createArticleNode(String title, File pdfFile) async {
   final rawContent = await pdfFile.readAsBytes();
   final pdfContent = String.fromCharCodes(rawContent);
