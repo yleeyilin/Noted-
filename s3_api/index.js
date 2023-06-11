d2b3afcda6e4c888fb903206dfa0a1aa89f7e436
Yi Lin <yiiiliiinn@gmail.com>
2023-06-11 23:17:31 +0800

    remove aws

--- s3_api/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-
-app.use(express.json());
-app.use("/api", require("./routes/app.routes"));
-
-app.listen(3000, function() {
-    console.log("AWS Server Ready!");
-})
--- s3_api/routes/app.routes.js
+++
@@ -1,8 +0,0 @@
-const uploadController = require("../controllers/upload.controller");
-
-const express = require("express");
-const router = express.Router();
-
-router.post("/image-upload", uploadController.uploadFile);
-
-module.exports = router;
