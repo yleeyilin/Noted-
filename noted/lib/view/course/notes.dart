57046e7449054e2706dd4e812621e68c8fd72895
kayabuttertoastt <e0970063@u.nus.edu>
2023-06-20 21:45:25 +0800

    updated notes

--- noted/lib/view/course/notes.dart
+++ noted/lib/view/course/notes.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:noted/view/constant/colors.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:noted/main.dart';
 import 'package:noted/view/widgets/generalsearchbar.dart';
 import 'package:noted/view/widgets/skeleton.dart';
-
-//need to link to database to get the notes
-//filter the notes posted based on the module code stated when the file in uploaded
+import 'package:noted/model/query/pdfviewerscreen.dart';
 
 class Notes extends StatefulWidget {
   final String courseCode;
@@ -18,6 +18,72 @@ class Notes extends StatefulWidget {
 }
 
 class _NotesState extends State<Notes> {
+  late List<Map<String, dynamic>> filteredNotes = [];
+  late List<Map<String, dynamic>> searchResults = [];
+
+  Future<void> fetchNotes() async {
+    final QueryOptions options = QueryOptions(
+      document: gql('''
+        query GetNotes(\$courseCode: String!) {
+          courses(where: { name: \$courseCode }) {
+            notes {
+              name
+              address
+            }
+          }
+        }
+      '''),
+      variables: <String, dynamic>{
+        'courseCode': widget.courseCode,
+      },
+    );
+
+    final QueryResult result = await client.value.query(options);
+
+    if (result.hasException) {
+      print('GraphQL Error: ${result.exception.toString()}');
+    } else {
+      final dynamic data = result.data?['courses'];
+      if (data != null) {
+        final List<dynamic> notesData = data[0]['notes'] as List<dynamic>;
+        setState(() {
+          filteredNotes = List<Map<String, dynamic>>.from(notesData);
+          searchResults = List<Map<String, dynamic>>.from(notesData);
+        });
+      }
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchNotes();
+  }
+
+  void searchNotes(String query) {
+    setState(() {
+      if (query.isNotEmpty) {
+        searchResults = filteredNotes
+            .where((note) => note['name']
+                .toString()
+                .toLowerCase()
+                .contains(query.toLowerCase()))
+            .toList();
+      } else {
+        searchResults = List<Map<String, dynamic>>.from(filteredNotes);
+      }
+    });
+  }
+
+  void viewPDF(String pdfUrl) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => PdfViewerScreen(pdfUrl: pdfUrl),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -41,6 +107,7 @@ class _NotesState extends State<Notes> {
             ),
           ],
         ),
+        //general search bar
         actions: [
           IconButton(
             onPressed: () {
@@ -70,11 +137,11 @@ class _NotesState extends State<Notes> {
               ),
             ),
             const SizedBox(height: 20),
-            //notes search bar
+            // Notes search bar
             SizedBox(
               height: 60,
               child: TextField(
-                onChanged: (value) {},
+                onChanged: (value) => searchNotes(value),
                 decoration: InputDecoration(
                   filled: true,
                   fillColor: Colors.white,
@@ -95,6 +162,54 @@ class _NotesState extends State<Notes> {
               ),
             ),
             const SizedBox(height: 20),
+            Expanded(
+              child: searchResults.isNotEmpty
+                  ? ListView.builder(
+                      itemCount: searchResults.length,
+                      itemBuilder: (context, index) {
+                        final note = searchResults[index];
+                        return Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              padding: const EdgeInsets.symmetric(
+                                vertical: 20,
+                                horizontal: 24,
+                              ),
+                              minimumSize: const Size(double.infinity, 48),
+                              backgroundColor: Colors.white,
+                              foregroundColor: primary,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(8),
+                                side: BorderSide(color: primary),
+                              ),
+                            ),
+                            onPressed: () {
+                              viewPDF(note['address']);
+                            },
+                            child: Text(
+                              note['name'],
+                              style: const TextStyle(
+                                fontSize: 17,
+                                fontWeight: FontWeight.bold,
+                                color: Color.fromARGB(255, 9, 9, 82),
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                    )
+                  : const Center(
+                      child: Text(
+                        'No Results Found',
+                        style: TextStyle(
+                          color: Color.fromARGB(255, 9, 9, 82),
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+            ),
           ],
         ),
       ),
