cb429b163b6b4e766974c9b145dee543dc51436c
Yi Lin <yiiiliiinn@gmail.com>
2023-06-29 16:38:52 +0800

    upgraded ui

--- noted/lib/view/login/signin.dart
+++ noted/lib/view/login/signin.dart
@@ -44,132 +44,123 @@ class _SignIn extends State<SignIn> {
               height: 100,
             ),
             const SizedBox(height: 200),
-            const Center(
-              child: Text(
-                'NUS Email',
-                style: TextStyle(
-                  color: Color.fromARGB(255, 9, 9, 82),
-                  fontSize: 20,
-                ),
-              ),
-            ),
             const SizedBox(height: 5),
             SizedBox(
-              width: 300,
-              child: CupertinoTextField(
-                controller: emailController,
-                placeholder: "Enter NUS Email",
-                textAlign: TextAlign.center,
-                padding:
-                    const EdgeInsets.symmetric(vertical: 10, horizontal: 30),
-                decoration: BoxDecoration(
-                  color: inputbox,
-                  borderRadius: BorderRadius.circular(9),
-                ),
-              ),
-            ),
-            const SizedBox(height: 20),
-            const Center(
-              child: Text(
-                'Password',
-                style: TextStyle(
-                  color: Color.fromARGB(255, 9, 9, 82),
-                  fontSize: 20,
-                ),
-              ),
-            ),
-            const SizedBox(height: 5),
-            SizedBox(
-              width: 300,
-              child: CupertinoTextField(
-                controller: passwordController,
-                padding:
-                    const EdgeInsets.symmetric(vertical: 10, horizontal: 50),
-                decoration: BoxDecoration(
-                  color: inputbox,
-                  borderRadius: BorderRadius.circular(9),
-                ),
-                obscureText: !_obscurePassword,
-                placeholder: "Enter Password",
-                textAlign: TextAlign.center,
-                suffix: GestureDetector(
-                  onTap: () {
-                    setState(() {
-                      _obscurePassword = !_obscurePassword;
-                    });
-                  },
-                  child: Icon(
-                    _obscurePassword ? Icons.visibility : Icons.visibility_off,
-                    color: Colors.grey,
-                  ),
-                ),
-              ),
-            ),
-            const SizedBox(height: 5),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                TextButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const ForgetPassword(),
-                      ),
-                    );
-                  },
-                  child: const Text(
-                    'Forgot Password?',
-                    style: TextStyle(
-                      fontSize: 15,
-                      color: Color.fromARGB(255, 9, 9, 82),
-                      fontStyle: FontStyle.italic,
+                width: 300,
+                child: TextField(
+                  controller: emailController,
+                  style: const TextStyle(color: Color.fromARGB(255, 9, 9, 82)),
+                  decoration: const InputDecoration(
+                    labelText: 'NUS Email',
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(color:Color.fromARGB(255, 9, 9, 82)),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color:Color.fromARGB(255, 9, 9, 82)), 
                     ),
                   ),
+                )
+              ),
+            const SizedBox(height: 20),
+            SizedBox(
+                width: 300,
+                child: TextField(
+                  controller: passwordController,
+                  style: const TextStyle(color: Color.fromARGB(255, 9, 9, 82)),
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    focusedBorder: const OutlineInputBorder(
+                      borderSide: BorderSide(color:Color.fromARGB(255, 9, 9, 82)), 
+                    ),
+                    border: const OutlineInputBorder(),
+                    suffixIcon: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          _obscurePassword = !_obscurePassword;
+                        });
+                      },
+                      child: Icon(
+                        _obscurePassword
+                            ? Icons.visibility
+                            : Icons.visibility_off,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ),
+                  obscureText: _obscurePassword,
                 ),
-              ],
-            ),
+              ),
+            const SizedBox(height: 5),
+            Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 30),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    TextButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const ForgetPassword(),
+                          ),
+                        );
+                      },
+                      child: const Text(
+                        'Forgot Password?',
+                        style: TextStyle(
+                          fontSize: 15,
+                          color: Color.fromARGB(255, 9, 9, 82),
+                          fontStyle: FontStyle.italic,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+             ),
             const SizedBox(
               height: 20,
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                OutlinedButton.icon(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => const Login()),
-                    );
-                  },
-                  style: OutlinedButton.styleFrom(
-                    foregroundColor: const Color.fromARGB(255, 9, 9, 82),
-                    side: const BorderSide(
-                      color: Color.fromARGB(255, 9, 9, 82),
+            Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 30),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    OutlinedButton.icon(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => const Login()),
+                        );
+                      },
+                      style: OutlinedButton.styleFrom(
+                        foregroundColor: const Color.fromARGB(255, 9, 9, 82),
+                        side: const BorderSide(
+                          color: Color.fromARGB(255, 9, 9, 82),
+                        ),
+                      ),
+                      icon: const Icon(Icons.arrow_back),
+                      label: const Text('Back'),
                     ),
-                  ),
-                  icon: const Icon(Icons.arrow_back),
-                  label: const Text('Back'),
-                ),
-                OutlinedButton.icon(
-                  onPressed: () {
-                    _con.signin(
-                        emailController.text, passwordController.text, context);
-                  },
-                  style: OutlinedButton.styleFrom(
-                    backgroundColor: const Color.fromARGB(255, 9, 9, 82),
-                  ),
-                  icon: const Icon(Icons.arrow_right_outlined,
-                      color: Colors.white),
-                  label: const Text(
-                    'Submit',
-                    style: TextStyle(
-                      color: Colors.white,
+                    OutlinedButton.icon(
+                      onPressed: () {
+                        _con.signin(
+                            emailController.text, passwordController.text, context);
+                      },
+                      style: OutlinedButton.styleFrom(
+                        backgroundColor: const Color.fromARGB(255, 9, 9, 82),
+                      ),
+                      icon: const Icon(Icons.arrow_right_outlined,
+                          color: Colors.white),
+                      label: const Text(
+                        'Submit',
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
                     ),
-                  ),
-                ),
-              ],
-            )
+                  ],
+                )
+            ),
           ],
         ),
       ),
