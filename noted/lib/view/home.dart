3bd6935fcf6cc6771f0348ff0dd21a22429fd68d
kayabuttertoastt <e0970063@u.nus.edu>
2023-07-01 15:19:19 +0800

    developed like function

--- noted/lib/view/home.dart
+++ noted/lib/view/home.dart
@@ -12,8 +12,9 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  List<Map<String, dynamic>> articles = [];
-  List<Map<String, dynamic>> allArticles = [];
+  List<Map<String, dynamic>>? articles;
+  List<Map<String, dynamic>>? allArticles;
+  List<Map<String, dynamic>> likedArticles = [];
   final ArticleController _con = ArticleController();
 
   @override
@@ -24,109 +25,148 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-        future: _con.fetchAllArticles(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const CircularProgressIndicator();
-          } else if (snapshot.hasError) {
-            return Text('Error: ${snapshot.error}');
-          } else {
-            dynamic data = snapshot.data;
+      future: _con.fetchAllArticles(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const CircularProgressIndicator();
+        } else if (snapshot.hasError) {
+          return Text('Error: ${snapshot.error}');
+        } else {
+          dynamic data = snapshot.data;
+          articles ??= [];
+          allArticles ??= [];
+          likedArticles = [];
+          if (data != null) {
             articles = List<Map<String, dynamic>>.from(data);
             allArticles = List<Map<String, dynamic>>.from(data);
-            return Scaffold(
-              body: Column(
-                children: [
-                  const SizedBox(height: 10),
-                  SizedBox(
-                    height: 60,
-                    child: TextField(
-                      onChanged: (value) {
-                        setState(() {
-                          if (value.isEmpty) {
-                            articles =
-                                List<Map<String, dynamic>>.from(allArticles);
-                          } else {
-                            articles = allArticles
-                                .where((article) => article['title']
-                                    .toLowerCase()
-                                    .contains(value.toLowerCase()))
-                                .toList();
-                          }
-                        });
-                      },
-                      decoration: InputDecoration(
-                        filled: true,
-                        fillColor: Colors.white,
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(8.0),
-                          borderSide:
-                              const BorderSide(color: Colors.transparent),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(8.0),
-                          borderSide: const BorderSide(
-                            color: Color.fromARGB(255, 9, 9, 82),
-                          ),
-                        ),
-                        hintText: 'Search',
-                        prefixIcon: const Icon(Icons.search),
-                        prefixIconColor: primary,
+            likedArticles = List<Map<String, dynamic>>.from(data);
+          }
+          return Scaffold(
+            body: Column(
+              children: [
+                const SizedBox(height: 10),
+                SizedBox(
+                  height: 60,
+                  child: TextField(
+                    onChanged: (value) {
+                      setState(() {
+                        if (value.isEmpty) {
+                          articles =
+                              List<Map<String, dynamic>>.from(allArticles!);
+                        } else {
+                          articles = allArticles!
+                              .where((article) => article['title']
+                                  .toLowerCase()
+                                  .contains(value.toLowerCase()))
+                              .toList();
+                        }
+                      });
+                    },
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                        borderSide: const BorderSide(color: Colors.transparent),
                       ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                        borderSide: const BorderSide(
+                          color: Color.fromARGB(255, 9, 9, 82),
+                        ),
+                      ),
+                      hintText: 'Search',
+                      prefixIcon: const Icon(Icons.search),
+                      prefixIconColor: primary,
                     ),
                   ),
-                  const SizedBox(height: 20),
-                  Expanded(
-                    child: articles.isEmpty
-                        ? const Center(
-                            child: Text(
-                              'No Results Found',
-                              style: TextStyle(
-                                color: Color.fromARGB(255, 9, 9, 82),
-                                fontSize: 22,
-                                fontWeight: FontWeight.bold,
-                              ),
+                ),
+                const SizedBox(height: 20),
+                Expanded(
+                  child: articles!.isEmpty
+                      ? const Center(
+                          child: Text(
+                            'No Results Found',
+                            style: TextStyle(
+                              color: Color.fromARGB(255, 9, 9, 82),
+                              fontSize: 22,
+                              fontWeight: FontWeight.bold,
                             ),
-                          )
-                        : ListView.builder(
-                            itemCount: articles.length,
-                            itemBuilder: (BuildContext context, int index) {
-                              final article = articles[index];
-                              return Card(
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(8.0),
-                                ),
-                                elevation: 2,
-                                margin: const EdgeInsets.symmetric(
-                                    horizontal: 16, vertical: 8),
-                                child: ListTile(
-                                  title: Text(
-                                    article['title'] as String,
-                                  ),
-                                  subtitle: Text(
-                                    article['summary'] as String,
-                                  ),
-                                  onTap: () {
-                                    _con.viewPDF(article['address'], context);
-                                  },
-                                  trailing: IconButton(
-                                    icon: Icon(
-                                      Icons.comment,
-                                      color: primary,
-                                    ),
-                                    onPressed: () {
-                                      _con.openCommentScreen(context);
-                                    },
-                                  ),
-                                ),
-                              );
-                            },
                           ),
-                  ),
-                ],
-              ),
-            );
-          }
-        });
+                        )
+                      : ListView.builder(
+                          itemCount: articles!.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            final article = articles![index];
+                            final isLiked = likedArticles.indexWhere(
+                                    (likedArticle) =>
+                                        likedArticle['id'] == article['id']) !=
+                                -1;
+                            final likeCount =
+                                article['like'] ?? 0; // Like count
+                            return Card(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(8.0),
+                              ),
+                              elevation: 2,
+                              margin: const EdgeInsets.symmetric(
+                                  horizontal: 16, vertical: 8),
+                              child: ListTile(
+                                title: Text(article['title'] as String),
+                                subtitle: Text(article['summary'] as String),
+                                onTap: () {
+                                  _con.viewPDF(article['address'], context);
+                                },
+                                trailing: Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    IconButton(
+                                      icon: Icon(
+                                        isLiked
+                                            ? Icons.favorite
+                                            : Icons.favorite_border,
+                                        color: primary,
+                                      ),
+                                      onPressed: () {
+                                        setState(() {
+                                          if (isLiked) {
+                                            likedArticles.removeWhere(
+                                                (likedArticle) =>
+                                                    likedArticle['id'] ==
+                                                    article['id']);
+                                          } else {
+                                            likedArticles.add(article);
+                                          }
+                                        });
+                                      },
+                                    ),
+                                    Text(
+                                      '$likeCount',
+                                      style: const TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    IconButton(
+                                      icon: Icon(
+                                        Icons.comment,
+                                        color: primary,
+                                      ),
+                                      onPressed: () {
+                                        _con.openCommentScreen(context);
+                                      },
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                ),
+              ],
+            ),
+          );
+        }
+      },
+    );
   }
 }
