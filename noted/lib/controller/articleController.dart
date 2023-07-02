8f2d7f3716e8ae7b9072a5504c3cce5fd07a293e
kayabuttertoastt <e0970063@u.nus.edu>
2023-07-03 00:49:05 +0800

    updated articles

--- noted/lib/controller/articleController.dart
+++ noted/lib/controller/articleController.dart
@@ -3,6 +3,8 @@ import 'package:mvc_pattern/mvc_pattern.dart';
 import 'package:noted/model/neo4j/retrieve.dart';
 import 'package:noted/model/query/pdfviewerscreen.dart';
 import 'package:noted/view/widgets/comment.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:noted/main.dart';
 
 class ArticleController extends ControllerMVC {
   factory ArticleController() => _this ??= ArticleController._();
@@ -29,5 +31,34 @@ class ArticleController extends ControllerMVC {
         builder: (context) => const Comment(),
       ),
     );
+  }
+
+  Future<void> updateLikeCount(String noteId, int newLikeCount) async {
+    const mutation = r'''
+    mutation UpdateLikeCount($noteId: ID!, $newLikeCount: Int!) {
+      updateNoteLikeCount(noteId: $noteId, newLikeCount: $newLikeCount) {
+        id
+        like
+      }
+    }
+  ''';
+
+    final MutationOptions options = MutationOptions(
+      document: gql(mutation),
+      variables: <String, dynamic>{
+        'noteId': noteId,
+        'newLikeCount': newLikeCount,
+      },
+    );
+
+    try {
+      final QueryResult result = await client.value.mutate(options);
+
+      if (result.hasException) {
+        print('GraphQL Error: ${result.exception.toString()}');
+      }
+    } catch (e) {
+      print('Error updating like count: $e');
+    }
   }
 }
