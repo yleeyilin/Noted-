3a9de4de0198461913ec8adb6ca66d3f3ecb5f25
kayabuttertoastt <e0970063@u.nus.edu>
2023-07-15 12:54:47 +0800

    developed function

--- noted/lib/model/neo4j/updateNode.dart
+++ noted/lib/model/neo4j/updateNode.dart
@@ -1,7 +1,7 @@
 import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:noted/main.dart';
 
-//updates the like count
+//updates the like count for articles
 Future<void> updateLikeCount(String address, int likeCount) async {
   const mutation = r'''
   mutation UpdateLikeCount($address: String!, $likeCount: Int!) {
@@ -14,6 +14,43 @@ Future<void> updateLikeCount(String address, int likeCount) async {
       }
     ) {
       articles {
+        likeCount
+      }
+    }
+  }
+''';
+  final MutationOptions options = MutationOptions(
+    document: gql(mutation),
+    variables: <String, dynamic>{
+      'address': address,
+      'likeCount': likeCount,
+    },
+  );
+
+  try {
+    final QueryResult result = await client.value.mutate(options);
+
+    if (result.hasException) {
+      print('GraphQL Error: ${result.exception.toString()}');
+    }
+  } catch (e) {
+    print('Error updating like count: $e');
+  }
+}
+
+//updates the like count for notes
+Future<void> updateLikeCountNotes(String address, int likeCount) async {
+  const mutation = r'''
+  mutation UpdateLikeCountNotes($address: String!, $likeCount: Int!) {
+    updateNotes(
+      where: {
+        address: $address
+      }
+      update: {
+        likeCount: $likeCount
+      }
+    ) {
+      notes {
         likeCount
       }
     }
