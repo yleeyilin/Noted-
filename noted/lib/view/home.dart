b82beb283624768b10902cfa8eebebbf5e5bee04
kayabuttertoastt <e0970063@u.nus.edu>
2023-07-16 18:45:39 +0800

    minor fix

--- noted/lib/view/home.dart
+++ noted/lib/view/home.dart
@@ -275,8 +275,7 @@ class _HomeState extends State<Home> {
                                         color: primary,
                                       ),
                                       onPressed: () {
-                                        _con.openCommentScreen(context,
-                                            article['id']?.toString() ?? '');
+                                        //to edit
                                       },
                                     ),
                                   ],
